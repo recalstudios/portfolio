85383ff9c056c91965ac0b3b9f4f0b33df1de0c9
soni801 <32430506+soni801@users.noreply.github.com>
2021-06-15 19:15:24 +0200

    Add keywords to subheader

--- scripts/common.js
+++ scripts/common.js
@@ -1,1 +1,49 @@
+// Store constants
+const slogan = "we make ";
+const keywords = ["homepages", "portfolios", "landing pages", "the web."];
+const sloganHolder = document.querySelector("#slogan");
+const keywordHolder = document.querySelector("#keyword");
+
+// Load assets
 $("#logo").load("/assets/icons/graphide-logo.svg");
+
+// Animate slogan
+setTimeout(function ()
+{
+    write(slogan, sloganHolder);
+}, 2500);
+
+setTimeout(function ()
+{
+    keywords.forEach(function (item, index)
+    {
+        setTimeout(function ()
+        {
+            write(item, keywordHolder);
+            if (index !== keywords.length - 1) setTimeout(function ()
+            {
+                erase(keywordHolder);
+            }, 3000);
+        }, index * 5000);
+    });
+}, 4000);
+
+// Functions
+function write(word, holder)
+{
+    let iteration = holder.innerHTML.length;
+    holder.innerHTML = word.substr(0, iteration + 1);
+    if (++iteration < word.length) setTimeout(function ()
+    {
+        write(word, holder);
+    }, 130);
+}
+
+function erase(holder)
+{
+    holder.innerHTML = holder.innerHTML.substr(0, holder.innerHTML.length - 1);
+    if (holder.innerHTML.length > 0) setTimeout(function ()
+    {
+        erase(holder);
+    }, 130);
+}
